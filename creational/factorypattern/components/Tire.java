16122d904f5ce10a2440fad93339a90dae5d9cd4
Jan-Luca Kingler <152486882+JanLucaKingler@users.noreply.github.com>
2025-10-07 22:29:54 +0200

    Create Tire.java

---
+++ creational/factorypattern/components/Tire.java
@@ -0,0 +1,14 @@
+package creational.factorypattern.components;
+
+public class Tire {
+    private int size;
+
+    public Tire(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
