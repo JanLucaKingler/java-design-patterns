2aa3ee46067c1baf0c379d46b5003538839dd1c6
kingler <janlucakingler@gmail.com>
2025-10-23 18:44:52 +0200

    change name for Factory-Main-Method

--- creational/factorypattern/FactoryMain.java
+++ creational/factorypattern/FactoryMain.java
@@ -3,7 +3,7 @@ package creational.factorypattern;
 import creational.factorypattern.car.Car;
 import creational.factorypattern.factory.CarFactory;
 
-public class Main {
+public class FactoryMain {
     static void main(String[] args) {
         Car sportCar = CarFactory.createCar(CarFactory.CarType.SPORT);
         sportCar.assemble();
