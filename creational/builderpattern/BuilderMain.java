b26f4cc76e62695111caf08907386411d65593d9
kingler <janlucakingler@gmail.com>
2025-10-23 18:44:18 +0200

    change name for Builder-Main-Method

---
+++ creational/builderpattern/BuilderMain.java
@@ -0,0 +1,19 @@
+import creational.builderpattern.Computer;
+
+ void main() {
+    Computer gamingPC = new Computer.Builder("Intel i9", 32)
+            .storage(2000)
+            .gpu("NVIDIA RTX 4080")
+            .operatingSystem("Windows 11")
+            .wifi(false)
+            .bluetooth(true)
+            .build();
+
+    Computer officePC = new Computer.Builder("AMD Ryzen 5", 16)
+            .storage(512)
+            .operatingSystem("Ubuntu Linux")
+            .build();
+
+    System.out.println(gamingPC);
+    System.out.println(officePC);
+}
